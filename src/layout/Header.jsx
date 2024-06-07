653fca2eb41da393841623191e00cb7fbd6c52ce
Leejunhyeok03 <ly0609@naver.com>
2024-06-07 12:11:01 +0900

    로고 경로 변경

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -53,7 +53,7 @@ export default function Header() {
         <LogoWrap>
           <Logo>
             <a href="/">
-              <Img src="/public/img/logo.png" alt="크림로고" />
+              <Img src="/img/logo.png" alt="크림로고" />
             </a>
           </Logo>
 
